6ca59d3f16d24098a9e89ec33ad88434726e50e6
김시우 <120652126+ksw110@users.noreply.github.com>
2023-01-09 17:50:21 +0900

    Add files via upload

---
+++ Ex_221102/src/Ex10_for문_별짓기4.java
@@ -0,0 +1,15 @@
+
+public class Ex10_for문_별짓기4 {
+
+	public static void main(String[] args) {
+
+		
+		for(int k = 5 ; k >= 1 ; k--) {
+			
+			for(int i = 1 ; i <= k ; i++) {
+				System.out.print("*");
+			}System.out.println();
+		}
+	}
+
+}
