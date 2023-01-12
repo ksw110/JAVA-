dd42db32ca35e76de47b2158310cdb8fb0f4f0cc
김시우 <120652126+ksw110@users.noreply.github.com>
2023-01-12 19:49:47 +0900

    Add files via upload

---
+++ Ex_221108/src/ex_1108/Ex02_변수있고리턴없고.java
@@ -0,0 +1,18 @@
+package ex_1108;
+
+public class Ex02_변수있고리턴없고 {
+
+	public static void main(String[] args) {
+
+		// sumPrint 메소드 이용해서
+		// 3과 5를 더한 결과 출력하기
+		sumPrint(3,5);
+		
+	}
+
+	public static void sumPrint(int a, int b) {
+		System.out.println(a + b);
+	
+		
+	}
+}
---
+++ Ex_221108/src/ex_1108/Ex03_변수없고리턴있고.java
@@ -0,0 +1,16 @@
+package ex_1108;
+
+public class Ex03_변수없고리턴있고 {
+
+	public static void main(String[] args) {
+
+		System.out.println(getName());
+		
+	}
+	public static String getName() {
+		return "김시우";
+		
+	}
+	
+	
+}
